28e275d384c545f32e5f61471ad03179717505df
Paulina Smaruj <smaruj@usc.edu>
2026-02-19 18:28:47 -0800

    removing old files and unused experiments

--- mouse_screening/run_screening.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python run_screening.py \
-  --fragments /scratch1/smaruj/mouse_screening/chr11_69057142_69276687_fragments.tsv \
-  --backgrounds /scratch1/smaruj/background_generation/background_sequences_scd30_totvar1300.fasta \
-  --genome /project2/fudenber_735/genomes/mm10/mm10.fa \
-  --model /home1/smaruj/pytorch_akita/model_0_v2_finetuned_correctly.pt \
-  --out /scratch1/smaruj/mouse_screening/chr11_screening_results_all_bg.tsv
